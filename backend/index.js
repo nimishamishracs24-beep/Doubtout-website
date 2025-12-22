6bfe65e9521e591176594c8fc05c9c3b00d66fa2
nimishamishracs24-beep <nimishamishra.cs24@bmsce.ac.in>
2025-12-22 19:22:44 +0530

    Use environment variable for server port

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const { connectDb } = require('./db');
 const { signUpUser, loginUser } = require('./authService');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 /* ---------------- MIDDLEWARE ---------------- */
 app.use(cors({ origin: '*'}));
